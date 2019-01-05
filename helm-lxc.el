c8bfe16d31a328df7487cdddcf682883c83908b2
montag451 <montag451@laposte.net>
2019-01-05 20:55:41 +0100

    Make package-lint happy

--- helm-lxc.el
+++ helm-lxc.el
@@ -25,6 +25,15 @@
 ;; You should have received a copy of the GNU General Public License
 ;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
+;;; Commentary:
+;;
+;; `helm-lxc' provides a Helm interface to manage your LXC containers.
+;;
+;; With `helm-lxc' you can list the containers on your local machine
+;; and on remote machines.  You can start, stop, restart, freeze,
+;; unfreeze your containers.  You can also start a shell inside a
+;; container.
+
 ;;; Code:
 
 (require 'helm)
